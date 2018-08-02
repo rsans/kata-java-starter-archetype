82fa9cfd2a2e3669989b8b1aa828d1204dcdb42f
rsans <rbn.sans@gmail.com>
2018-08-02 17:01:42 +0200

    First version of kata java starter archetype

---
+++ src/main/resources/archetype-resources/src/test/java/TemplateTest.java
@@ -0,0 +1,25 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TemplateTest {
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void should_write_code_to_test() {
+		assertTrue(true);
+	}
+
+}
